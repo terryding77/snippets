12dbac6e74d0d4cb7d218e342272c4a141c76ab1
terryding77 <terryding77@gmail.com>
2017-09-06 20:13:18 +0800

    change prime ret

--- python/prime.py
+++ python/prime.py
@@ -6,15 +6,15 @@ def is_prime(x):
             return False
     return True
 
-def get_prime_list(n):
-    prime_list = []
+def get_prime_set(n):
+    prime_set = set()
     prime_mark = [False, False] + [True for _ in range(n - 1)]
     for base in range(2, n + 1):
         if prime_mark[base]: 
-            prime_list.append(base)
-        for prime in prime_list:
+            prime_set.add(base)
+        for prime in prime_set:
             composite = prime * base
             if composite > n:
                 break
             prime_mark[composite] = False 
-    return prime_list
+    return prime_set
